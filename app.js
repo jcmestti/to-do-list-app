0f8986723e97ed3c77c2781316b2376441dc2e45
jcmestti <jcmestti@gmail.com>
2022-03-19 17:48:34 -0300

    Tasks can now be added to the to do list

---
+++ app.js
@@ -0,0 +1,19 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const addNewTask = newTask => {
+    const newLi = `
+            <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+                <span>${newTask}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>
+    `;
+
+    list.innerHTML += newLi;
+};
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newTask = addForm.add.value.trim();
+    addNewTask(newTask); 
+}); 
